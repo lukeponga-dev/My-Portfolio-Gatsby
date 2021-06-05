e4177f48336c972c4771faed32c020f08389d2a6
Luke Devops <developmentdesignsltd@gmail.com>
2021-06-05 22:32:52 +1200

    Add Secound project

--- src/mock/data.js
+++ src/mock/data.js
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: 'Luke Ponga | Developer', // e.g: 'Name | Developer'
+  title: 'Luke Ponga | Portfolio', // e.g: 'Name | Developer'
   lang: 'en', // e.g: en, es, fr, jp
   description: 'Software Developer', // e.g: Welcome to my website
 };
@@ -34,20 +34,21 @@ export const projectsData = [
     id: nanoid(),
     img: 'aotearoanz.online.png',
     title: 'New Zealand Website',
-    info: 'an assignment i was given whne i was studying web design and development',
+    info: 'An Assignment I was given while i was studying web design and development',
     info2: '',
     url: 'https://aotearoanz.online/',
     repo: 'https://github.com/therealponga1/New-Zealand-Website', // if no repo, the button will not show up
   },
-  //  {
-  //   id: nanoid(),
-  //   img: 'project.jpg',
-  //   title: '',
-  //   info: '',
-  //   info2: '',
-  //   url: '',
-  //   repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  // },
+  
+   {
+    id: nanoid(),
+    img: 'project2.png',
+    title: 'Doctor Appointments',
+    info: 'I developed doctors appointmnets for one of my assignments in software development',
+    info2: 'Doctor appointments is a web app using the asp.net core framework. The Purpose of this web app is to provid patients with an easy appointment booking systems',
+    url: 'https://doctorsappointments.azurewebsites.net/',
+    repo: 'https://github.com/therealponga1/Doctors_Appointments_DSD06', // if no repo, the button will not show up
+  },
   // {
   //   id: nanoid(),
   //   img: 'project.jpg',
